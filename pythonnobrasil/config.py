061df550c317f913efb48584bc7f25342597961e
Marco Rougeth <marco@rougeth.com>
2018-03-05 19:50:14 -0300

    Reorganize project structure

--- pythonnobrasil/config.py
+++ pythonnobrasil/config.py
@@ -1,4 +1,8 @@
+from pathlib import Path
+
 from decouple import config
+
+BASE_DIR = Path().parent / 'pythonnobrasil'
 
 GOOGLE_API_AUTH = config('GOOGLE_API_AUTH', default='google_auth.json')
 GOOGLE_API_CALENDAR_ID = config('GOOGLE_API_CALENDAR_ID')
